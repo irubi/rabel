28c4c8da58eb70ea31484b521b9490a596718b16
irubi <elite.ljp@gmail.com>
2013-07-18 17:37:50 +0800

    precompile path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rabel::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += [ Proc.new {|path| File.basename(path).start_with?('i_')} ]
+  config.assets.precompile += [ Proc.new {|path| File.basename(path) =~ /^[^_].*\.\w+$/} ]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
